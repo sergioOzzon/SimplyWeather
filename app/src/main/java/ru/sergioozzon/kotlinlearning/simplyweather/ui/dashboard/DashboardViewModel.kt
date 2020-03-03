b28d6c863b0da09377f9cfc4c1b92d022aa7bee3
SergioOzzon <ser2603@yandex.ru>
2020-03-03 18:39:22 +0300

    add value and home_layout in resources

---
+++ app/src/main/java/ru/sergioozzon/kotlinlearning/simplyweather/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package ru.sergioozzon.kotlinlearning.simplyweather.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/ru/sergioozzon/kotlinlearning/simplyweather/ui/home/HomeFragment.kt
@@ -0,0 +1,32 @@
+package ru.sergioozzon.kotlinlearning.simplyweather.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_home.*
+import ru.sergioozzon.kotlinlearning.simplyweather.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            text_home.text = it
+        })
+        return root
+    }
+}
